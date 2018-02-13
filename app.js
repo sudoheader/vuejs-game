3beef57768c4e325f834c9c629bd80f70402a51b
Raymond Nazaryan <rayn2427@gmail.com>
2018-02-12 22:37:57 -0800

    Added heal method implementation

--- app.js
+++ app.js
@@ -28,7 +28,12 @@ new Vue({
             this.monsterAttacks();
         },
         heal: function() {
-
+            if (this.playerHealth <= 90) {
+                this.playerHealth += 10;
+            } else {
+                this.playerHealth = 100;
+            }
+            this.monsterAttacks();
         },
         giveUp: function () {
             
